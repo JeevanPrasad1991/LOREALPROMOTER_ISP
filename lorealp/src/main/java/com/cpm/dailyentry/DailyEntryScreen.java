4c30c992ced9945d732f0e0300af81fdeffa3a83
upendrak <upendra.kushwaha@in.cpm-int.com>
2018-02-01 15:10:47 +0530

    upendra_

--- lorealp/src/main/java/com/cpm/dailyentry/DailyEntryScreen.java
+++ lorealp/src/main/java/com/cpm/dailyentry/DailyEntryScreen.java
@@ -68,6 +68,7 @@ public class DailyEntryScreen extends AppCompatActivity implements OnItemClickLi
     FloatingActionButton fab_button;
     boolean flag_deviation = false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
